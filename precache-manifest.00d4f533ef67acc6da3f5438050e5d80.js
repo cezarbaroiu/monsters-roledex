d90812e037adaf6a1809bfb98d71059e99b147ea
cezar.baroiu <cezar.baroiu@gmail.com>
2020-08-09 12:22:06 +0300

    Updates

---
+++ precache-manifest.00d4f533ef67acc6da3f5438050e5d80.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db4d22be1401603f9e44eee9a7fa903b",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "69e024403abeef60b0c8",
+    "url": "/monsters-rolodex/static/css/main.6b54940c.chunk.css"
+  },
+  {
+    "revision": "82815bfcb9bf73d8a971",
+    "url": "/monsters-rolodex/static/js/2.be6af004.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.be6af004.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69e024403abeef60b0c8",
+    "url": "/monsters-rolodex/static/js/main.502267ab.chunk.js"
+  },
+  {
+    "revision": "46fcc5578023f1586c35",
+    "url": "/monsters-rolodex/static/js/runtime-main.7affd505.js"
+  }
+]);
